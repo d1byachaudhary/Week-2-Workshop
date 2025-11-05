d5f44c77a8e3d2dfc61544184406f2e9c6a55d26
Asus <Asus@DIBYA>
2025-11-05 09:39:01 +0545

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Task 1
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string userName = "Dibya";
+        int luckyNumber = 3;
+
+        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+    }
+}
